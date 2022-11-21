e65d257b5853cf43ecbda248718d77000977d068
huangzefeng <737244264@qq.com>
2022-11-21 21:37:40 +0800

    安装 swagger 并集成到 gin 中

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	docs "ginchat/docs"
 	"ginchat/router"
 	"ginchat/util"
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func main() {
@@ -10,6 +13,8 @@ func main() {
 	util.MysqlInit()
 	r := router.Route()
 
+	docs.SwaggerInfo.BasePath = ""
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 	//test.UserTest()
 }
--- service/userService.go
+++ service/userService.go
@@ -8,6 +8,11 @@ import (
 	"strconv"
 )
 
+// @Tags 用户数据
+// @Accept json
+// @Produce json
+// @Success 200 {string} Helloworld
+// @Router /userList [get]
 func UserService(c *gin.Context) {
 	page, _ := strconv.Atoi(c.Query("page"))
 	pageSize, _ := strconv.Atoi(c.Query("pageSize"))
